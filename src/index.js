2ee0c34b311744aa2afeef8678b181cb39e98b76
Marvin Botchway <marvinbotchway@outlook.com>
2024-01-26 12:15:48 +0000

    Add Webpack
    Added and set up config files and other boilerplate code.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement("p");
+
+  element.textContent = "Hello World Test";
+
+  return element;
+}
+
+document.body.appendChild(component());
