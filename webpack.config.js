23c1c832dec179621d1cffea9b6d4ce7cd7f7837
Marvin Botchway <marvinbotchway@outlook.com>
2024-02-09 15:22:02 +0000

    Add webpack-dev-server And Correct Typo

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
   },
   module: {
     rules: [
